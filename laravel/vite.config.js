bf704ca9899d7736a91fef93d6c8a0a60eeacde4
DiegoAM7 <diiego.am7@gmail.com>
2023-04-22 19:07:57 -0400

    Add Vite, React and Inertia

--- laravel/vite.config.js
+++ laravel/vite.config.js
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: "resources/js/app.jsx",
             refresh: true,
+            buildDirectory: "../public_html",
+            publicDirectory: "../public_html",
+            hotFile: "../public_html/hot",
         }),
+        react({}),
     ],
 });
