e7322bed49fd386f6ebdd67010d280a89470f6ee
DiegoAM7 <diiego.am7@gmail.com>
2023-04-22 23:54:24 -0400

    Correction indent

--- laravel/resources/js/Components/TextInput.jsx
+++ laravel/resources/js/Components/TextInput.jsx
@@ -1,25 +1,22 @@
-import { forwardRef, useEffect, useRef } from 'react';
+import { forwardRef, useEffect, useRef } from 'react'
 
 export default forwardRef(function TextInput({ type = 'text', className = '', isFocused = false, ...props }, ref) {
-    const input = ref ? ref : useRef();
+	const input = ref ? ref : useRef()
 
-    useEffect(() => {
-        if (isFocused) {
-            input.current.focus();
-        }
-    }, []);
+	useEffect(() => {
+		if (isFocused) {
+			input.current.focus()
+		}
+	}, [])
 
-    return (
-        <div className="flex flex-col items-start">
-            <input
-                {...props}
-                type={type}
-                className={
-                    'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm ' +
-                    className
-                }
-                ref={input}
-            />
-        </div>
-    );
-});
+	return (
+		<div className='flex flex-col items-start'>
+			<input
+				{...props}
+				type={type}
+				className={'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm ' + className}
+				ref={input}
+			/>
+		</div>
+	)
+})
